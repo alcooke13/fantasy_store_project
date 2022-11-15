874eef4b4482a25813cdfb0c868fd61a57c27aec
Alistair <alistair13_cooke@hotmail.com>
2022-11-15 19:06:39 +0000

    added working potions html page with stock counter

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -71,3 +71,23 @@ def count_weapons(weapons):
         if "Shield" in weapon.name:
             shield_count += 1
     return [sword_count, axe_count, spear_count, shield_count]
+
+def select_potions(products):
+    potions_list = []
+    for product in products:
+        if product.product_type.name == "Potions":
+            potions_list.append(product)
+    return potions_list
+
+def count_potions(potions):
+    health_count = 0
+    mana_count = 0
+    strength_count = 0
+    for potion in potions:
+        if "Health" in potion.name:
+            health_count += 1
+        if "Mana" in potion.name:
+            mana_count += 1
+        if "Strength" in potion.name:
+            strength_count += 1
+    return [health_count, mana_count, strength_count]
